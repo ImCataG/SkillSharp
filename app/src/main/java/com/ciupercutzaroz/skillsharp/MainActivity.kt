b4055f12884f03ef44e8a95a9e52ef497de04638
imcatag <imcatag@gmail.com>
2023-11-11 17:14:42 +0200

    first UI exists!

--- app/src/main/java/com/ciupercutzaroz/skillsharp/MainActivity.kt
+++ app/src/main/java/com/ciupercutzaroz/skillsharp/MainActivity.kt
@@ -11,19 +11,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ciupercutzaroz.skillsharp.ui.theme.SkillSharpTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+//            SkillSharpTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }            }
             SkillSharpTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }            }
+                val navController = rememberNavController()
+                MainScreen(navController = navController)
+            }
         }
     }
 }
@@ -41,5 +55,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     SkillSharpTheme {
         Greeting("Android")
+    }
+}
+
+// Create a main screen with a title and a list of items
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "SkillSharp",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+        )
+        LazyColumn {
+            items(listOf("Item 1", "Item 2", "Item 3")) { item ->
+                Text(
+                    text = item,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+        }
     }
 }
